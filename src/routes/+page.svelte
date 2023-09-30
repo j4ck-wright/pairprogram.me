91facc325117470ac4165e9745575cef18feec8c
Jack <jackw221001@Gmail.com>
2023-09-30 20:49:07 +0100

    websocket dev and server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+	import { io } from 'socket.io-client';
+
+	const socket = io();
+
+	socket.on('eventFromServer', (message) => {
+		console.log(message);
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <h2 class="text-red-500">Now with added Tailwind CSS</h2>
