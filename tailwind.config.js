60a22047b294e3268a109da8dd74d233f810c30c
Jack <jackw221001@Gmail.com>
2023-10-05 19:37:33 +0100

    themes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px'
+		},
+		fontFamily: {
+			sans: ['Lato', 'sans-serif']
+		},
 		extend: {}
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ['light']
+		darkTheme: 'light',
+		themes: [
+			{
+				light: {
+					primary: '#FF8A00',
+					secondary: '#1c1917',
+					accent: '#854d0e',
+					neutral: '#1e1523',
+					'base-100': '#f5f5f4',
+					info: '#98d6f0',
+					success: '#14a374',
+					warning: '#df8e16',
+					error: '#e34a61'
+				}
+			},
+			{
+				dark: {
+					primary: '#FF8A00',
+					secondary: '#1c1917',
+					accent: '#854d0e',
+					neutral: '#f5f5f4',
+					'base-100': '#070421',
+					info: '#98d6f0',
+					success: '#14a374',
+					warning: '#df8e16',
+					error: '#e34a61'
+				}
+			}
+		]
 	}
 };
