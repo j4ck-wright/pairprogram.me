13f755518cc36052de2d1b2c077de44537431ac8
Jack <jackw221001@Gmail.com>
2023-10-05 21:38:48 +0100

    feat: navbar, theme toggler

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+	import ThemeToggle from '$components/ThemeToggle.svelte';
 </script>
 
+<nav class="flex justify-between mx-4 mt-2">
+	<h2>pairprogram.me</h2>
+	<ThemeToggle />
+</nav>
 <slot />
