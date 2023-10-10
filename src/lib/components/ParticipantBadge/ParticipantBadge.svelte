7c6512dbdf4977f980db1b98c045cbaf21f7b543
Jack <jackw221001@Gmail.com>
2023-10-10 16:46:03 +0100

    feat: Participant components added

---
+++ src/lib/components/ParticipantBadge/ParticipantBadge.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import 'iconify-icon';
+	import { createEventDispatcher } from 'svelte';
+
+	export let participant: string;
+
+	const dispatch = createEventDispatcher();
+
+	function removeParticipant() {
+		dispatch('removeParticipant', {
+			participant: participant
+		});
+	}
+</script>
+
+<div class="badge badge-lg badge-primary gap-2 text-inherit">
+	<iconify-icon
+		icon="iconoir:cancel"
+		role="button"
+		tabindex="0"
+		class="cursor-pointer"
+		on:click={removeParticipant}
+		on:keydown={removeParticipant}
+	/>
+	<span class="capitalize">{participant}</span>
+</div>
