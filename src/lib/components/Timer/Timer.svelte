60ad98b5dc681a003360ce33106072595b073043
Jack <jackw221001@Gmail.com>
2023-10-13 17:19:25 +0100

    skeleton structure

---
+++ src/lib/components/Timer/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+	let percentage = 80;
+</script>
+
+<div class="container">
+	<div class="timer-container flex justify-center mr-6">
+		<div
+			class="radial-progress text-primary font-bold text-4xl"
+			style="--value:{percentage}; --size:24rem; --thickness: 0.5rem;"
+		>
+			10:00
+		</div>
+	</div>
+
+	<div class="buttons mt-6 flex justify-center gap-10">
+		<button>placeholder 1</button>
+		<button>placeholder 2</button>
+		<button>placeholder 3</button>
+	</div>
+</div>
