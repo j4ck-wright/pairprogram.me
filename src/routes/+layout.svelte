e4a76e2006b17efbe7bfbb09ad85e3f14dcc1848
Jack Wright <58521043+j4ck-wright@users.noreply.github.com>
2023-11-03 12:22:09 +0000

    feat: dark mode toggle (#58)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+	import { browser } from '$app/environment';
+	function querySelector(state: 'light' | 'dark') {
+		if (browser) {
+			document?.querySelector('html')?.setAttribute('data-theme', state);
+		}
+	}
+	let light = true;
+	$: light ? querySelector('light') : querySelector('dark');
 </script>
 
 <svelte:head>
 	<title>pairprogram.me</title>
 </svelte:head>
+
+<nav class="flex justify-between items-center p-2">
+	<a href="/">pairprogram.me</a>
+	<input
+		type="checkbox"
+		class="toggle toggle-primary toggle-sm"
+		checked
+		on:click={() => {
+			light = !light;
+		}}
+	/>
+</nav>
 
 <slot />
